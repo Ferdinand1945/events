f74cff895cd53ff3095e01732124b6b2c32b5d2c
fernando <flopez@iolabs.se>
2024-09-05 23:10:09 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '700'],
+  variable: '--font-poppins'
+ });
+
+export const metadata: Metadata = {
+  title: "Sastreria",
+  description: "Eventos, entradas y mucho mas",
+  icons: {
+    icon: '/assets/images/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher([
+    '/sign-in(.*)', 
+    '/sign-up(.*)', 
+    '/(.*)',
+    '/events/:id(.*)',
+    '/api/webhook/clerk(.*)',
+    '/api/webhook/stripe(.*)',
+    '/api/uploadthing(.*)',])
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+})
