b483c1d4ea8db262b5fb77e27cee49f256f7789c
fernando <flopez@iolabs.se>
2024-09-09 14:21:48 +0200

    added page for events

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import {SearchParamProps } from '@/types'
+import { getEventById } from '@/lib/actions/event.actions'
+import Image from 'next/image'
 
-const page = () => {
+const EventDetails = async ({params: {id}}: SearchParamProps) => {
+const event = await getEventById(id)
+console.log(event)
   return (
-    <div>page</div>
+    <section className='flex justify-center bg-primary-50 bg-dotted-pattern'>
+        <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
+            <Image src={event.imageUrl}
+            alt='hero image'
+            width={1000}
+            height={1000}
+            className='h-full min-h-[300px] object-cover object-center' 
+            />
+             <div className='flex w-full flex-col gap-8 p-5 md:p-10'>
+                <div className='flex flex-col gap-6'>
+                    <h2 className='h2-bold'>{event.title}</h2>
+                    <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
+                        <div className='flex gap-3'>
+                            <p className='p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700'>
+                                {event.isFree ? 'FREE' : `€${event.price}`}
+                            </p>
+                            <p className='p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500'>
+                                {event.category.name}
+                            </p>
+                        </div>
+                        <p className='p-medium-18 ml-2 mt-2 sm:mt-0'>
+                            by{' '}
+                            <span className='text-primary-500'>{event.organizer.firstName} {event.organizer.lastName}</span>
+                        </p>
+                    </div>
+                </div>
+
+                {/**checkout button */}
+             </div>
+        </div>
+    </section>
   )
 }
 
-export default page
+export default EventDetails
